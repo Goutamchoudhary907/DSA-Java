e6c651b9e5e60b176ad39e24658c729ac6b6c6c4
Goutam Choudhary <goutamchoudhary907@gmail.com>
2024-03-13 21:02:36 +0530

    Commit

--- Sorting/GFG Problems/BinaryArraySort.java
+++ Sorting/GFG Problems/BinaryArraySort.java
@@ -1,7 +1,7 @@
-public class BInSort {
+public class BinaryArraySort {
     static void binSort(int A[], int N) {
         int i = 0, j = N - 1;
-        while (i > j) {
+        while (i < j) {
             if (A[i] == 1 && A[j] == 0) {
                 int temp=A[i];
                 A[i]=A[j];
@@ -9,10 +9,10 @@ public class BInSort {
                 i++;
                 j--;
             } 
-            while (A[i]==0) i++;
-            while(A[j]==1) j--;
-        }
+          if (A[i]==0) i++;
+           if(A[j]==1) j--;
     }
+    } 
     static void print(int arr[]) {
         for(int i:arr) {
             System.out.print(i+" ");
@@ -22,7 +22,7 @@ public class BInSort {
     public static void main(String[] args) {
         int[] A={1,0,1,1,0};
      print(A);
-     binSort(A, A.length);
+    binSort(A, A.length);
      print(A);
     }
 }
