705d8f4fc37c2273ec9743fc99273de78b82beca
Goutam Choudhary <goutamchoudhary907@gmail.com>
2024-03-29 17:36:17 +0530

    Commit

--- Linked List/Basics.java
+++ Linked List/Basics.java
@@ -6,9 +6,25 @@ public class Basics {
             this.data=data;
         }
     }
+    public static void display(Node head) {
+        while(head !=null) {
+            System.out.print(head.data+" ");
+            head=head.next;
+        }
+        System.out.println();
+     }
+
+     public static void insertAtEnd(Node head , int data) {
+        Node temp =new Node(data);
+        Node t= head;
+        while (t.next!=null) {
+            t=t.next;
+        }
+        t.next=temp;
+     }
     public static void main(String[] args) {
      Node a=new Node(5);
-     System.out.println(a.next);              // null
+    //  System.out.println(a.next);              // null
      Node b=new Node(3);
      Node c=new Node(9);
      Node d=new Node(8);
@@ -16,16 +32,20 @@ public class Basics {
      
      // 5  3 9 8 16
      a.next=b;              // 5 -> 3 9 8 16
-     System.out.println(a);                   //Basics$Node@28a418fc     This is address of a
-     System.out.println(a.next);              //Basics$Node@5305068a     This is address present in next of a => which is address of next node => which is b 
-     System.out.println(b);                   //Basics$Node@5305068a     This is address of b 
-     System.out.println(c);                   //Basics$Node@1f32e575     This is address of c
+    //  System.out.println(a);                   //Basics$Node@28a418fc     This is address of a
+    //  System.out.println(a.next);              //Basics$Node@5305068a     This is address present in next of a => which is address of next node => which is b 
+    //  System.out.println(b);                   //Basics$Node@5305068a     This is address of b 
+    //  System.out.println(c);                   //Basics$Node@1f32e575     This is address of c
     
-    System.out.println(b.data);
-    System.out.println(a.next.data);       // access data of b with using a
+    // System.out.println(b.data);
+    // System.out.println(a.next.data);       // access data of b with using a
    
    b.next=c;            // 5 -> 3 -> 9   8   16
    c.next=d;            // 5 -> 3 -> 9 ->8   16
    d.next=e;            // 5 -> 3 -> 9 ->8 ->16
+
+    display(a);
+    insertAtEnd(a,87);
+    display(a);
 }
 }
--- Linked List/Implementation.java
+++ Linked List/Implementation.java
@@ -9,15 +9,84 @@ public class Implementation {
         public static class linkedList {
         Node head=null;
         Node tail=null;
-        void insertAtEnd(int data) {
+        int size=0;  
+
+        void insertAtEnd(int data) {        // TC=O(1)
             Node temp=new Node(data);
             if(head==null) {
                 head=temp;
             } else{
                 tail.next=temp;
             }
-            tail=temp; 
+            tail=temp;
+            size++;                       // use this to increase size every time you insert element 
+                                          // by this we can calculate size in O(1) 
         } 
+
+        void insertAtBegin(int data) {      // TC=O(1)
+            Node temp=new Node(data);
+            if(head==null) {
+               head =tail=temp; 
+          //     insertAtEnd(data);   we can do this because in empty list end and start is same 
+            } else {
+                temp.next=head;
+                head=temp;
+            }
+            size++;
+        }
+
+        void inserAt(int idx,int data) {        //TC= O(n)  SC=O(1)
+            Node t=new Node(data);
+            Node temp=head;
+            if(idx==size()) {
+               insertAtEnd(data); 
+               return;
+            } else if(idx==0) {
+               insertAtBegin(data);
+               return; 
+            } else if(idx<0 || idx>size()) {
+                System.out.println("Wrong index");
+                return;
+            }
+            for(int i=0;i<idx-1;i++) {
+               temp=temp.next; 
+            }
+           t.next=temp.next;
+           temp.next=t; 
+           size++;
+        }
+
+        int getElementAt(int idx) {            // TC=O(n)
+            if(idx<0 || idx>size()) {
+                System.out.println("Wrong index");
+                return -1;
+            }
+            Node temp=head;
+           for(int i=0;i<idx;i++) {
+            temp=temp.next;
+           }
+           return temp.data;
+        }
+
+        void deleteAtIndex(int idx) {
+            if(idx<0 || idx>size()) {
+                System.out.println("wrong index");
+                return;
+            } else if(idx==0) {
+                head=head.next;
+                size--;
+                return;
+            }
+            Node temp=head;
+            for(int i=1;i<idx;i++) {
+                temp=temp.next;
+            }
+           temp.next= temp.next.next;
+           if(idx==size()) {
+            tail=temp;
+           }
+           size--;
+        }
       
         void print() {
             Node temp=head;
@@ -25,9 +94,10 @@ public class Implementation {
             System.out.print(temp.data+" ");
             temp=temp.next;
         }
+        System.out.println();
         }
 
-        int size() {
+        int size() {                   // TC=O(n)
             Node temp=head;
             int count=0;
             while(temp!=null) {
@@ -41,8 +111,25 @@ public class Implementation {
             linkedList l=new linkedList();
             l.insertAtEnd(4);
             l.insertAtEnd(5);
+            l.print(); 
+            // System.out.println("Size:"+l.size());
+            l.insertAtEnd(6);
             l.print();
-            System.out.println();
-            System.out.println("Size:"+l.size());
+            l.insertAtBegin(100);
+            l.print();
+            l.inserAt(4, 10);
+            l.print();
+            // System.out.println(l.tail.data);
+            l.inserAt(0, 13);
+            l.print();
+            int a=l.getElementAt(1);
+            System.out.println(a);
+            System.out.println(l.size);
+
+            l.print();
+            l.deleteAtIndex(0);
+            l.print();
+            System.out.println(l.tail.data);
+
         }
     }
